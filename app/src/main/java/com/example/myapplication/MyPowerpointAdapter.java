ddce72c078e5704118617069825455606c0b94a7
Stephani <47741146+stephaniemaaz@users.noreply.github.com>
2020-04-03 16:24:22 +0300

    added changes for mockup

--- app/src/main/java/com/example/myapplication/MyPowerpointAdapter.java
+++ app/src/main/java/com/example/myapplication/MyPowerpointAdapter.java
@@ -6,12 +6,15 @@ import android.widget.ImageView;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import androidx.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 import timber.log.Timber;
 
 public class MyPowerpointAdapter extends RecyclerView.Adapter {
 
-    private List<Powerpoint> mPowerPointList;
+    private List<String> mPowerPointList;
 
     static class PagesViewHolder extends RecyclerView.ViewHolder {
         private ImageView mViewImageView;
@@ -22,7 +25,7 @@ public class MyPowerpointAdapter extends RecyclerView.Adapter {
         }
     }
 
-    public MyPowerpointAdapter(List<Powerpoint> mPowerPointList) { this.mPowerPointList = mPowerPointList; }
+    public MyPowerpointAdapter(List<String> mPowerPointList) { this.mPowerPointList = mPowerPointList; }
 
     @NonNull
     @Override
@@ -35,8 +38,13 @@ public class MyPowerpointAdapter extends RecyclerView.Adapter {
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         Timber.i("On Bind Holder of My Pager Adapter");
         PagesViewHolder mViewHolder = (PagesViewHolder) holder;
-        Powerpoint powerpoint = mPowerPointList.get(position);
-        mViewHolder.mViewImageView.setImageResource(powerpoint.getTrialImage());
+        String imageUrl = mPowerPointList.get(position);
+        // set image
+        Glide.with(holder.itemView.getContext())
+                .load(imageUrl)
+                .centerCrop()
+                .error(R.drawable.default_background)
+                .into(mViewHolder.mViewImageView);
     }
 
     @Override
